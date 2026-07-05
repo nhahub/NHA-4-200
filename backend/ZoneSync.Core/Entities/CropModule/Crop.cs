03b0feca1903afbee13911ab4cca9133bcacfc31
Rana7assan <rvnvhassan@gmail.com>
2026-07-06 02:13:46 +0300

    Add Crop Class

---
+++ backend/ZoneSync.Core/Entities/CropModule/Crop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZoneSync.Core.Entities.CropModule
+{
+    internal class Crop
+    {
+        public int CropId { get; set; }
+        public string CropName { get; set; }
+        public string CropSeason { get; set; }
+        public string CropCategory { get; set; }
+        public string IrrigationType { get; set; }
+
+        // public ICollection<GrowthStage> GrowthStages { get; set; } = new List<GrowthStage>();
+        //public ICollection<CropPlan> CropPlans { get; set; } = new List<CropPlan>();
+
+    }
+}
